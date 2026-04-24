3861ab3580c6442b25a817761d16219d6b56596c
H2 VPN <h2@h2msg.org>
2026-04-24 17:16:54 +0000

    v3.0.3: fix swapped upload/download

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -287,16 +287,16 @@ class MainActivity : HelperBaseActivity(), NavigationView.OnNavigationItemSelect
                             serverStartUp = up
                             serverStartDown = down
                         }
-                        val totalUp = down - serverStartDown  // server down = client upload
-                        val totalDown = up - serverStartUp     // server up = client download
+                        val totalUp = up - serverStartUp       // up = client upload
+                        val totalDown = down - serverStartDown  // down = client download
                         binding.tvUploadTotal.text = formatBytes(totalUp)
                         binding.tvDownloadTotal.text = formatBytes(totalDown)
 
                         if (lastFetchTime > 0 && up >= serverLastUp && down >= serverLastDown) {
                             val dt = (now - lastFetchTime) / 1000.0
                             if (dt > 0.5) {
-                                val sUp = ((down - serverLastDown) / dt).toLong()
-                                val sDown = ((up - serverLastUp) / dt).toLong()
+                                val sUp = ((up - serverLastUp) / dt).toLong()
+                                val sDown = ((down - serverLastDown) / dt).toLong()
                                 speedUpHistory.add(sUp)
                                 speedDownHistory.add(sDown)
                                 if (speedUpHistory.size > 3) speedUpHistory.removeAt(0)
